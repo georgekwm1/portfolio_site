1eb785ba1ba77d6b46387ad77f518fb3debbd573
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-07-07 09:52:58 +0100

    Edits

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':  BASE_DIR / 'db.sqlite3',
-
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': '1amge0rge.',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
