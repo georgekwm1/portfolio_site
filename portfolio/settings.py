70618efc5a581dfdf28ba5097d5e88aa325d444b
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-09-01 17:32:16 +0100

    updated static files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -136,6 +136,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [
+    BASE_DIR / 'images'
+]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR
